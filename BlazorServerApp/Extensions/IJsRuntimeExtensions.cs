893e3315efe03bfaaa93c8f348a15124168f9fe7
kourosh <kourosh23@hotmail.com>
2024-11-15 12:58:32 +0100

    toastr

---
+++ BlazorServerApp/Extensions/IJsRuntimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorServerApp.Extensions
+{
+    public static class IJsRuntimeExtensions
+    {
+        public static async Task ToastrSuccess(this IJSRuntime jSRuntime, string message)
+        {
+            await jSRuntime.InvokeVoidAsync("showToastr", "success", message);
+        }
+
+        public static async Task ToastrWarning(this IJSRuntime jSRuntime, string message)
+        {
+            await jSRuntime.InvokeVoidAsync("showToastr", "warning", message);
+        }
+
+        public static async Task ToastrError(this IJSRuntime jSRuntime, string message)
+        {
+            await jSRuntime.InvokeVoidAsync("showToastr", "error", message);
+        }
+    }
+}
---
+++ BlazorServerApp/wwwroot/js/toastr.js
@@ -0,0 +1,11 @@
+﻿window.showToastr = function (type, message) {
+    if (type == "success") {
+        toastr.success(message);
+    }
+    else if (type == "error") {
+        toastr.error(message);
+    }
+    else if (type == "warning") {
+        toastr.warning(message);
+    }
+}
