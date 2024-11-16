1440cd9009f7f2e068eff9fe7cb548270d4d1811
kourosh <kourosh23@hotmail.com>
2024-11-16 09:58:57 +0100

    ApplicationDbContext

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Category> Categories { get; set; }
+}
