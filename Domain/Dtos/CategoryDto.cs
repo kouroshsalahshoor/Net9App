2ed92ad5b390446243527b6737a3a2cdeb924662
kourosh <kourosh23@hotmail.com>
2024-11-17 10:38:08 +0100

    Category CRUD

--- Domain/Dtos/CategoryDto.cs
+++ Domain/Dtos/CategoryDto.cs
@@ -1,7 +1,10 @@
-﻿namespace Domain.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Dtos;
 
 public class CategoryDto
 {
     public int Id { get; set; }
+    [Required(ErrorMessage ="please enter a {0}")]
     public string Name { get; set; }
 }
