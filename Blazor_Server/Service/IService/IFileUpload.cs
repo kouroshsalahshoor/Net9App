173b46d9cbbf2658c7a305bbb55b5a6dfebe3504
kourosh <kourosh23@hotmail.com>
2024-11-20 12:47:03 +0100

    product file

---
+++ Blazor_Server/Service/IService/IFileUpload.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace Blazor_Server.Service.IService;
+
+public interface IFileUpload
+{
+    Task<string> UploadFile(IBrowserFile browserFile);
+    bool DeleteFile(string filePath);
+}
