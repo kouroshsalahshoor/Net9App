ffaaf65aba9f28b9f9fd8f8c3169430db9ad73fa
kourosh <kourosh23@hotmail.com>
2024-11-25 20:00:56 +0100

    CurrentLocation

---
+++ Blazor_Wasm/wwwroot/js/currentLocation.js
@@ -0,0 +1,25 @@
+﻿//window.getLongitude = async () => {
+//    const pos = await new Promise((resolve, reject) => {
+//        navigator.geolocation.getCurrentPosition(resolve, reject);
+//    });
+
+//    return pos.coords.longitude;
+//};
+
+//window.getLatitude = async () => {
+//    const pos = await new Promise((resolve, reject) => {
+//        navigator.geolocation.getCurrentPosition(resolve, reject);
+//    });
+
+//    return pos.coords.latitude;
+//};
+
+window.getCoords = async () => {
+    const pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+
+    //return [pos.coords.longitude, pos.coords.latitude];
+    return pos.coords.latitude + ',' + pos.coords.longitude
+
+};
