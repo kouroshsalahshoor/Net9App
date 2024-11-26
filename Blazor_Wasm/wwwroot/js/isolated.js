80366f6146655fc7a291f38c3ce2cecce930f72e
kourosh <kourosh23@hotmail.com>
2024-11-26 12:03:55 +0100

    Isolated Js

---
+++ Blazor_Wasm/wwwroot/js/isolated.js
@@ -0,0 +1,7 @@
+﻿export function getLocalOffsetminutes() {
+    return new Date().getTimezoneOffset();
+}
+
+export function addToCart(productName) {
+    alert(productName + " has been added to Cart")
+}
