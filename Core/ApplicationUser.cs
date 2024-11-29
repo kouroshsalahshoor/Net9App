e43bb22610deea2b061eef40f139956f662617f3
kourosh <kourosh23@hotmail.com>
2024-11-29 12:54:00 +0100

    authorize

--- Core/ApplicationUser.cs
+++ Core/ApplicationUser.cs
@@ -6,6 +6,8 @@ namespace Core
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
     }
 
 }
