b4e9b4fe36ef68f66e9affe1e17ceaf831d21ac9
kourosh <kourosh23@hotmail.com>
2024-12-03 07:29:58 +0100

    seleniumtest

---
+++ SeleniumxUnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumxUnitTest
+{
+    public class UnitTest1
+    {
+        IWebDriver driver;
+
+        [Fact]
+        public void Test1()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl(new Uri("https://localhost:7176/categories"));
+        }
+    }
+}
